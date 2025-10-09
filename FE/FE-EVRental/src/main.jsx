e238792cc068278ebac2290198ae7b67eaad02ab
HuyNguyen48 <huynqse182305@fpt.edu.vn>
2025-10-09 11:26:57 +0700

    sửa lại responsity và home page

--- FE/FE-EVRental/src/main.jsx
+++ FE/FE-EVRental/src/main.jsx
@@ -6,6 +6,7 @@ import App from "./App.jsx";
 import "./index.css";
 import "./styles/scrollbar.css";
 import "./styles/antd-patch.css";
+import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
